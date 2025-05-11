8023ab71d0ba0f420b36bcbe7030cc7a05d68e45
Валентин ЧерныхÐ <chernykhvalentin@gmail.com>
2025-05-11 14:27:49 +0300

    Implemented IGuest and IGuests interfaces to reduce code coupling and improve architectural flexibility. Key changes: - Added the IGuest interface with methods for handling guest data (name, passport, email, etc.). - Implemented the IGuests interface with the add() method returning IGuest* and find() for passport-based search.

--- hotel-management-system/IGuest/iguest.h
+++ hotel-management-system/IGuest/iguest.h
@@ -3,20 +3,19 @@
 
 class IGuest {
 public:
-    virtual std::string name() const = 0;
-    virtual std::string surname() const = 0;
-    virtual std::string patronymic() const = 0;
-    virtual std::string passport() const = 0;
-    virtual std::string birth_date() const = 0;
-    virtual std::string email() const = 0;
+    virtual std::string passport() = 0;
+    virtual std::string name() = 0;
+    virtual std::string surname() = 0;
+    virtual std::string patronymic() = 0;
+    virtual std::string birth_date() = 0;
+    virtual std::string email() = 0;
     virtual void remove() const = 0;
-    virtual void save() const = 0;
 
+    virtual void change_passport(const std::string& new_passport) = 0;
     virtual void change_name(const std::string& new_name) = 0;
     virtual void change_surname(const std::string& new_surname) = 0;
     virtual void change_patronymic(const std::string& new_patronymic) = 0;
-    virtual void change_passport(const std::string& new_passport) = 0;
-    virtual void change_birth_date(const std::string& new_birth) = 0;
+    virtual void change_birth_date(const std::string& new_birth_date) = 0;
     virtual void change_email(const std::string& new_email) = 0;
 
     virtual ~IGuest() = default;
